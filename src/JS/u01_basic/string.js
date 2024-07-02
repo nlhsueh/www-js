a1723a4bd4676d23e20489c4c05cc7037edbd355
nienlinh <nlhsueh@mail.fcu.edu.tw>
2024-07-02 09:40:39 +0800

    ㄡ

--- src/JS/u01_basic/string.js
+++ src/JS/u01_basic/string.js
@@ -2,6 +2,8 @@ let s = 'hello, world';
 console.log(s);
 console.log(s.length);
 
+console.log(s.split('').reverse().join(''))
+
 let s2 = s.toUpperCase();
 console.log(s2)
 console.log(s)
@@ -11,5 +13,5 @@ console.log(s.includes('Hello'))
 // s2 = s[0].toUpperCase() + s.slice(1)
 // s2 = s.charAt(0).toUpperCase() + s.slice(1)
 s2 = s.substring(0).toUpperCase() + s.substring(1)
-s2 = s.capitalize()
+// s2 = s.capitalize()
 console.log(s2)

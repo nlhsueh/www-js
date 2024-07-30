26d3129a4412df21214548ffee63db09c5e95abe
nlh <nlhsueh@gmail.com>
2024-07-30 17:21:01 +0800

    .

---
+++ src/VUE/u01_basic/ttt/olympic.js
@@ -0,0 +1,246 @@
+
+
+https://s.yimg.com/iu/api/res/1.2/H5jufdpMK9qmxjuR3X6G7g--~B/YXBwaWQ9c2hhcmVkO2ZpPWZpbGw7cT0xMDA7aD02NDt3PTY0/https://s.yimg.com/cv/apiv2/default/soccer/flags/wwc/20190412/usa_300x300.png
+
+
+
+United States
+3	8	9	20
+France
+5	8	3	16
+Japan
+6	2	4	12
+China
+5	5	2	12
+Great Britain
+2	5	3	10
+Australia
+5	4	0	9
+Republic of Korea
+5	3	1	9
+Italy
+2	3	3	8
+Canada
+2	1	2	5
+Hong Kong
+2	0	1	3
+Kazakhstan
+1	0	2	3
+South Africa
+1	0	2	3
+Brazil
+0	1	2	3
+Sweden
+0	1	2	3
+Germany
+2	0	0	2
+Belgium
+1	0	1	2
+Turkey
+0	1	1	2
+India
+0	0	2	2
+Moldova
+0	0	2	2
+Azerbaijan
+1	0	0	1
+Romania
+1	0	0	1
+Serbia
+1	0	0	1
+Uzbekistan
+1	0	0	1
+Fiji
+0	1	0	1
+Kosovo
+0	1	0	1
+Mongolia
+0	1	0	1
+Poland
+0	1	0	1
+Tunisia
+0	1	0	1
+Croatia
+0	0	1	1
+Egypt
+0	0	1	1
+Hungary
+0	0	1	1
+Ireland
+0	0	1	1
+Mexico
+0	0	1	1
+Slovakia
+0	0	1	1
+Spain
+0	0	1	1
+Switzerland
+0	0	1	1
+Ukraine
+0	0	1	1
+Afghanistan
+0	0	0	0
+Albania
+0	0	0	0
+Algeria
+0	0	0	0
+American Samoa
+0	0	0	0
+Andorra
+0	0	0	0
+Angola
+0	0	0	0
+Antigua and Barbuda
+0	0	0	0
+Argentina
+0	0	0	0
+Armenia
+0	0	0	0
+Aruba
+0	0	0	0
+Austria
+0	0	0	0
+Bahamas
+0	0	0	0
+Bahrain
+0	0	0	0
+Bangladesh
+0	0	0	0
+Barbados
+0	0	0	0
+Belize
+0	0	0	0
+Benin
+0	0	0	0
+Bermuda
+0	0	0	0
+Bhutan
+0	0	0	0
+Bolivia
+0	0	0	0
+Bosnia and Herzegovina
+0	0	0	0
+Botswana
+0	0	0	0
+British Virgin Islands
+0	0	0	0
+Brunei
+0	0	0	0
+Bulgaria
+0	0	0	0
+38	
+Burkina Faso
+0	0	0	0
+38
+Burundi
+0	0	0	0
+38
+Cambodia
+0	0	0	0
+38
+Cameroon
+0	0	0	0
+Cape Verde
+0	0	0	0
+Cayman Islands
+0	0	0	0
+Central African Republic
+0	0	0	0
+Chad
+0	0	0	0
+Chile
+0	0	0	0
+Chinese Taipei
+0	0	0	0
+Colombia
+0	0	0	0
+Comoros
+0	0	0	0
+Congo
+0	0	0	0
+Cook Islands
+0	0	0	0
+Costa Rica
+0	0	0	0
+Cuba
+0	0	0	0
+Cyprus
+0	0	0	0
+Czech Republic
+0	0	0	0
+Denmark
+0	0	0	0
+Djibouti
+0	0	0	0
+Dominica
+0	0	0	0
+Dominican Republic
+0	0	0	0
+Democratic Republic of the Congo
+0	0	0	0
+38
+Ecuador
+0	0	0	0
+El Salvador
+0	0	0	0
+Equatorial Guinea
+0	0	0	0
+Eritrea
+0	0	0	0
+Estonia
+0	0	0	0
+Ethiopia
+0	0	0	0
+Micronesia
+0	0	0	0
+Finland
+0	0	0	0
+Gabon
+0	0	0	0
+Gambia
+0	0	0	0
+Georgia
+0	0	0	0
+Ghana
+0	0	0	0
+Greece
+0	0	0	0
+Grenada
+0	0	0	0
+Guam
+0	0	0	0
+Guatemala
+0	0	0	0
+Guinea
+0	0	0	0
+Guinea - Bissau
+0	0	0	0
+Guyana
+0	0	0	0
+Haiti
+0	0	0	0
+Honduras
+0	0	0	0
+Iceland
+0	0	0	0
+Individual Neutral Athletes
+0	0	0	0
+38
+Indonesia
+0	0	0	0
+Iran
+0	0	0	0
+Iraq
+0	0	0	0
+Israel
+0	0	0	0
+Cote d'Ivoire
+0	0	0	0
+Jamaica
+0	0	0	0
+Jordan
+0	0	0	0
+Kenya
+0	0	0	0
+Kiribati
+0	0	0	0
---
+++ src/VUE/u01_basic/ttt/test.md
@@ -0,0 +1,3 @@
+
+
+![](https://s.yimg.com/iu/api/res/1.2/H5jufdpMK9qmxjuR3X6G7g--~B/YXBwaWQ9c2hhcmVkO2ZpPWZpbGw7cT0xMDA7aD02NDt3PTY0/https://s.yimg.com/cv/apiv2/default/soccer/flags/wwc/20190412/usa_300x300.png)

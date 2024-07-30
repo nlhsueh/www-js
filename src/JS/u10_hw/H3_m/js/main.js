d8e1b6a827bffb230ce8a100c931df05df67de35
nienlinh <nlhsueh@mail.fcu.edu.tw>
2024-07-30 10:01:09 +0800

    .

--- src/JS/u10_hw/H3_m/js/main.js
+++ src/JS/u10_hw/H3_m/js/main.js
@@ -1,7 +1,8 @@
-function go(li){
+function go(li) {
     let href = window.location.href,
         url = href.split("#");
     if (url.length == 1) url.push(li.title);
     else url[1] = li.title;
     window.location.href = url.join("#");
+    console.log(window.location.href);
 }
